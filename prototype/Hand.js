b7d3819d08813695ee9156c94a0b5e0331980399
mancmelou <mancmelou@me.com>
2014-02-28 10:55:24 -0800

    Fixed tabs

--- prototype/Hand.js
+++ prototype/Hand.js
@@ -1,8 +1,8 @@
 var Hand = {
-	reset: function () {
-		this.actions = 1;
-		this.buys    = 1;
-		this.coins   = 0;
-		this.cards   = [];
-	}
+  reset: function () {
+    this.actions = 1;
+    this.buys    = 1;
+    this.coins   = 0;
+    this.cards   = [];
+  }
 };
